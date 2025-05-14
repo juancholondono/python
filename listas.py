7013295d1bb873e8f7243f7c406f6b7c853bfcad
juancholondono <juancholondono@gmail.com>
2025-05-13 19:52:11 -0500

    add dicionario

--- listas.py
+++ listas.py
@@ -10,7 +10,21 @@ def recorrer_vistas(l):
     Recorrer una lista de listas y mostrar los elementos en forma de tabla.
     """
     for i in range(len(l)):
-        for j in range(len(l[i])):
+     for j in range(len(l[i])):
             print(l[i][j], end="\t")
-        print()
+    print()
 recorrer_vistas(listas)
+
+
+dicionario = {
+    "nombre": "Juan",
+    "edad": 30,
+    "ciudad": "Madrid"
+}
+def recorrer_diccionario(d):
+    """
+    Recorrer un diccionario y mostrar las claves y valores.
+    """
+    for clave, valor in d.items():
+        print(f"{clave}: {valor}")
+recorrer_diccionario(dicionario)
